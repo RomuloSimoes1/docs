db1007f0f1eb7749b3edffff086aed1a57d79a0b
Romulo Simões <136924509+RomuloSimoes1@users.noreply.github.com>
2025-05-24 14:27:05 -0300

    Update js.js

--- js.js
+++ js.js
@@ -1,18 +1,18 @@
 let drawing = document.getElementById('drawing')
-let images = ['images/bart.jpeg', 'images/fish.jpeg']
+let img = ['img/bart.jpeg', 'img/fish.jpeg']
 let index = 0
 
 function draw() {
     drawing.innerHTML = ''
 
     // let photo = document.createElement('img');
-    // photo.setAttribute('src', images[index]);
+    // photo.setAttribute('src', img[index]);
     // photo.style.width = '200px'
 
     // drawing.appendChild(photo);
 
-    // index = (index + 1) % images.length;
-    images.forEach(function(imgUrl) {
+    // index = (index + 1) % img.length;
+    img.forEach(function(imgUrl) {
         const imgElement = document.createElement('img');
         imgElement.src = imgUrl;
         drawing.appendChild(imgElement);  // Adiciona a imagem ao container
